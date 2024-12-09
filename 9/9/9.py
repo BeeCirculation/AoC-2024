4fff0a89e5d86f5e22d7be397dd6c2afd16292dd
Name Nameson <HimeCirculation@users.noreply.github.com>
2024-12-09 08:11:41 +0000

    day 9 part 2

--- 9/9/9.py
+++ 9/9/9.py
@@ -1,3 +1,8 @@
+from os.path import isfile
+
+from more_itertools.more import first
+
+
 def parse(fp):
     with open(fp, "r") as f:
         return f.readline()[:-1]
@@ -50,13 +55,77 @@ def checksum(disk):
         sum += i * block
     return sum
 
+
+def part1(disk):
+    disk_ = disk.copy()
+    reorder(disk_)
+    sum = checksum(disk_)
+    print(sum)
+
+
+def partition(disk):
+    out = [[]]
+
+    ID_last = "none"
+    for ID in disk:
+        if ID == ID_last or ID_last == "none":
+            out[-1].append(ID)
+        else:
+            out.append([ID])
+
+        ID_last = ID
+    return out
+
+
+def defrag(disk: list):
+    disk_ = disk.copy()
+
+    i = len(disk_)
+    while i >= 0:
+        i -= 1
+        last_file = disk_[i]
+        if last_file[0] is None:
+            continue
+
+        for k in range(i):
+            first_empty = disk_[k]
+            if first_empty[0] is not None:
+                continue
+            if len(first_empty) < len(last_file):
+                continue
+
+            if len(first_empty) == len(last_file):
+                swap(disk_, i, k)
+            else:
+                diff = len(first_empty) - len(last_file)
+                split1 = [None] * len(last_file)
+                split2 = [None] * diff
+                disk_.pop(k)
+                disk_.insert(k, split1)
+                disk_.insert(k+1, split2)
+                i += 1
+                swap(disk_, k, i)
+            break
+    return disk_
+
+
+def departition(disk):
+    out = []
+    for part in disk:
+        for block in part:
+            out.append(block)
+    return out
+
+def part2(disk):
+    disk_ = disk.copy()
+    parts = partition(disk_)
+    defragged = departition(defrag(parts))
+    return checksum(defragged)
+
+
 disk_map = parse("input")
 #disk_map = parse("test")
 
 decompressed = decompress(disk_map)
-reorder(decompressed)
-sum = checksum(decompressed)
-print(sum)
 
-
-
+print(part2(decompressed))
