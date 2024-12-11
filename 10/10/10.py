88603f481c785583d617ab437c1884bba53f21c7
Name Nameson <HimeCirculation@users.noreply.github.com>
2024-12-11 10:33:25 +0000

    day 10 part 2 : Added a bool to switch between finding unique paths

--- 10/10/10.py
+++ 10/10/10.py
@@ -3,7 +3,7 @@ def parse(fp):
         return [line[:-1] for line in file.readlines()]
 
 
-def get_score(grid, position):
+def get_score(grid, position, unique=True):
     directions = [(1,0), (0,1), (-1,0), (0,-1)]
     MAX = 9
     x, y = position
@@ -12,13 +12,17 @@ def get_score(grid, position):
     if start == MAX:
         return {(x, y)}
 
-    score = set()
+    score = set() if unique else []
     for d in directions:
         next_x, next_y = x + d[0], y + d[1]
         if 0 <= next_x < len(grid) and 0 <= next_y < len(grid[0]):
             next = int(grid[next_x][next_y])
             if next == start + 1:
-                score.update(get_score(grid, (next_x, next_y)))
+                if unique:
+                    score.update(get_score(grid, (next_x, next_y), unique))
+                else:
+                    score.extend(get_score(grid, (next_x, next_y), unique))
+
 
     return score
 
@@ -31,7 +35,7 @@ sum = 0
 for i, line in enumerate(map):
     for k, char in enumerate(line):
         if char == "0":
-            score = len(get_score(map, (i, k)))
+            score = len(get_score(map, (i, k), False))
             print(score)
             sum += score
 print(sum)
