605358290a0a1c456df978a42825cecbbfaef11d
Name Nameson <HimeCirculation@users.noreply.github.com>
2024-12-12 07:35:22 +0000

    day 11 part 2. re-write to cache values

--- 11/11.py
+++ 11/11.py
@@ -38,15 +38,28 @@ rules = [((lambda x: x == 0), rule1),
 
 #stones = [125, 17]
 
-for k in range(75):
-    i = 0
-    # Each while loop is one blink
-    while i < len(stones):
-        print(k, f"{i} / {len(stones)}")
-        for condition, rule in rules:
-            if condition(stones[i]):
-                stones, i = rule(stones, i)
-                break
-        i += 1
-print(stones)
-print(len(stones))
+def Cache(func):
+    _cache = {}
+
+    def wrapper(*args):
+        if args in _cache:
+            return _cache[args]
+
+        _cache[args] = func(*args)
+        return _cache[args]
+    return wrapper
+
+@Cache
+def count(stone, steps):
+    if steps == 0:
+        return 1
+    if stone == 0:
+        return count(1, steps - 1)
+    string = str(stone)
+    length = len(string)
+    if length % 2 == 0:
+        return count(int(string[: length // 2]), steps - 1) + count(int(string[length // 2:]), steps - 1)
+    return count(stone * 2024, steps - 1)
+
+
+print(sum([count(stone, 75) for stone in stones]))
