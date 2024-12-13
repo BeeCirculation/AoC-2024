5ba0ba5ae24339e63093f228f7fc76e031d85fa5
Bee <HimeCirculation@users.noreply.github.com>
2024-12-13 09:29:02 +0000

    day 12 part 2. Added Region methods: _find_sides, sides, _find_side

--- 12/12.py
+++ 12/12.py
@@ -17,6 +17,7 @@ class Region:
         self.grid = area
         self.plots = set()
         self.types = set()
+        self.edges = set()
 
     def __isvalid(self, coord):
         return 0 <= coord[0] < len(self.grid) and 0 <= coord[1] < len(self.grid[0])
@@ -47,18 +48,79 @@ class Region:
                         self.plots.add(adj)
             q.pop(0)
 
-    def perimeter(self):
+    def _find_perimeter(self):
         # For each plot, calculate the number of edges that dont boarder another plot in the region
+        # Store the edges (as a pair of plots)
         count = 0
         for plot in self.plots:
             for direction in Region.directions:
                 adj = plot[0] + direction[0], plot[1] + direction[1]
                 if adj not in self.plots:
-                    count += 1
-        return count
+                    self.edges.add((plot, adj))
+        return self.edges
+
+    def perimeter(self):
+        self._find_perimeter()
+        return len(self.edges)
 
     def area(self):
         return len(self.plots)
+
+    def _find_sides(self):
+        """
+        Finds all the sides of the region. Self._find_perimeter() must be called first to ensure the edges are up to date
+        Returns (list[set[tuple[int, int]]]): A list of sides which are represented as sets of edges which are pairs
+        of plots which are tuples of a pair of integers
+
+        """
+        sides_list = []
+        seen_edges = set()
+        for edge in self.edges:
+            if edge not in seen_edges:
+                side = self._find_side(edge)
+                seen_edges.update(side)
+                sides_list.append(side)
+        return sides_list
+
+    def sides(self):
+        return len(self._find_sides())
+
+    def _find_side(self, edge: tuple[tuple[int,int], tuple[int,int]]):
+        plot, outside = edge
+        plot_y, plot_x = plot
+        outside_y, outside_x = outside
+
+        # A side is formed by adjacent edges. Edges are adjacent if they are next to eachother and in the same direction
+        # So we must find the direction of the edge and check its neighbours
+
+        # The direction vector of plot to outside determines the edge direction
+        edge_direction_y, edge_direction_x = outside_y - plot_y, outside_x - plot_x
+
+        # The edge is perpendicular to the direction vector between the plots that define it
+        if abs(edge_direction_y) == 1:
+            directions = [(0, 1), (0,-1)]
+        else:
+            directions = [(1,0), (-1,0)]
+
+        # Now based on these directions we must flood fill the edges to find the side
+        side = {edge}
+        q = [edge]
+        while len(q) > 0:
+            current = q[0]
+            plot, outside = current
+            plot_y, plot_x = plot
+            outside_y, outside_x = outside
+            for direction in directions:
+                adj_plot = plot_y + direction[0], plot_x + direction[1]
+                adj_outside = outside_y + direction[0], outside_x + direction[1]
+                adj = adj_plot, adj_outside
+
+                if adj in self.edges:
+                    if adj not in side:
+                        side.add(adj)
+                        q.append(adj)
+            q.pop(0)
+        return side
 
 
 seen_plots = set()
@@ -69,7 +131,9 @@ for i,line in enumerate(garden):
             continue
         r = Region(garden)
         r.fill((i,k))
-        price += r.perimeter() * r.area()
+        perimeter = r.perimeter()
+        sides = r.sides()
+        price += r.sides() * r.area()
 
         seen_plots.update(r.plots)
 print(price)
