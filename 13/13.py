775fe30e5e804a0269ca1fb4291cac3bd04e223c
Bee <HimeCirculation@users.noreply.github.com>
2024-12-14 11:05:17 +0000

    vector class and loading input data

--- 13/13.py
+++ 13/13.py
@@ -1,1 +1,42 @@
+class Vector:
+    def __init__(self, data):
+        self.data = data
+        self.dimension = len(data)
 
+    def __add__(self, other: "Vector"):
+        if self.dimension != other.dimension:
+            raise ValueError
+
+        return Vector([a + b for a, b in zip(self.data, other.data)])
+
+    def __mul__(self, other):
+        if not isinstance(other, int):
+            raise ValueError
+
+        return Vector([a * other for a in self.data])
+
+    def __str__(self):
+        return str(self.data).replace("[", "<").replace("]", ">")
+
+def parse(fp):
+    with open(fp, "r") as file:
+        line = file.readline()
+        output = [{}]
+        i = 0
+        while line:
+            if line == "\n":
+                line = file.readline()
+                i += 1
+                output.append({})
+                continue
+            label_data = line.split(": ")
+            label = label_data[0]
+            data = [int(num[2:]) for num in label_data[1].split(", ")]
+            output[i].update({label: Vector(data)})
+            line = file.readline()
+
+    return output
+
+
+machines = parse("test")
+print(machines)
