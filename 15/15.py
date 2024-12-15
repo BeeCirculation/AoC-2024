94ef6e689fdc9b202764594a0c5f5f663fb5d6f8
Bee <HimeCirculation@users.noreply.github.com>
2024-12-15 11:00:12 +0000

    added more operators to Vector

--- 15/15.py
+++ 15/15.py
@@ -11,12 +11,66 @@ class Vector:
             raise ValueError
 
         return Vector([a + b for a, b in zip(self.data, other.data)])
+    def __iadd__(self, other):
+        self.data = (self + other).data
+        return self
+    def __radd__(self, other):
+        return self + other
 
-    def __mul__(self, other):
-        if not isinstance(other, int):
+    def __sub__(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            return Vector([a - other for a in self.data])
+
+        if self.dimension != other.dimension:
             raise ValueError
 
+        return Vector([a - b for a, b in zip(self.data, other.data)])
+    def __isub__(self, other):
+        self.data = (self - other).data
+        return self
+    def __rsub__(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            return Vector([other - a for a in self.data])
+        else:
+            raise TypeError
+
+    def __mul__(self, other):
+        if not isinstance(other, int) and not isinstance(other, float):
+            raise TypeError
+
         return Vector([a * other for a in self.data])
+    def __imul__(self, other):
+        self.data = (self * other).data
+        return self
+    def __rmul__(self, other):
+        return self * other
+
+    def __truediv__(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            return Vector([a / other for a in self])
+        else:
+            raise TypeError
+    def __itruediv__(self, other):
+        self.data = (self / other).data
+        return self
+    def __rtruediv__(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            return Vector([other / a for a in self])
+        else:
+            raise TypeError
+    def __floordiv__(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            return Vector([a // other for a in self])
+        else:
+            raise TypeError
+    def __ifloordiv__(self, other):
+        self.data = (self // other).data
+        return self
+    def __rfloordiv__(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            return Vector([other // a for a in self])
+        else:
+            raise TypeError
 
     def __mod__(self, other):
         if isinstance(other, int):
@@ -26,6 +80,24 @@ class Vector:
             raise ValueError("None conformable Vector objects")
 
         return Vector([a % b for a,b in zip(self, other)])
+    def __imod__(self, other):
+        self.data = (self % other).data
+        return self
+    def __rmod__(self, other):
+        if isinstance(other, int):
+            return Vector([other % a for a in self])
+        else:
+            raise TypeError
+
+    def __round__(self, n=None):
+        return Vector([round(a, n) for a in self])
+    def __ceil__(self):
+        return Vector([a.__ceil__() for a in self])
+    def __floor__(self):
+        return Vector([a.__floor__() for a in self])
+
+    def __len__(self):
+        return self.dimension
 
     def __iter__(self):
         self.index = 0
