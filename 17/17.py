7accf1003b1ad156f3c0048bef01a424752f842e
Bee <HimeCirculation@users.noreply.github.com>
2024-12-19 23:55:35 +0000

    Added operations

--- 17/17.py
+++ 17/17.py
@@ -8,3 +8,65 @@ def parse(fp):
     return a, b, c, program
 
 A, B, C, program = parse("test")
+IC = 0
+output = []
+
+operands = {0: 0,
+            1: 1,
+            2: 2,
+            3: 3,
+            4: A,
+            5: B,
+            6: C,
+            7: None}
+
+def adv(op):
+    global A
+    A = A / pow(2, operands[op])
+    return 2
+
+def bxl(op):
+    global B
+    B = B ^ op
+    return 2
+
+def bst(op):
+    global B
+    B = operands[op] % 8
+    return 2
+
+def jnz(op):
+    global A, IC
+    if A == 0:
+        return 2
+
+    IC = op
+    return 0
+
+def bxc(op):
+    global B, C
+    B = B ^ C
+    return 2
+
+def out(op):
+    global output
+    output.append(operands[op] % 8)
+    return 2
+
+def bdv(op):
+    global B, A
+    B = A / pow(2, operands[op])
+    return 2
+
+def cdv(op):
+    global A, C
+    A = C / pow(2, operands[op])
+
+opcodes = {0: adv,
+           1: bxl,
+           2: bst,
+           3: jnz,
+           4: bxc,
+           5: out,
+           6: bdv,
+           7: cdv}
