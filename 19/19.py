7060004fc14ce29d24a708c011de2de3f784063e
Bee <HimeCirculation@users.noreply.github.com>
2024-12-21 10:53:52 +0000

    removed unnrecessary towels list in function

--- 19/19.py
+++ 19/19.py
@@ -1,10 +1,16 @@
+from functools import cache
+
+
 def parse(fp):
     with open(fp, "r") as file:
         towels, patterns = file.read().split("\n\n")
 
     towels = towels.split(", ")
     patterns = patterns.split("\n")
-    return towels, patterns[:-1]
+    return set(towels), patterns[:-1]
+
+
+towels, patterns = parse("input")
 
 
 def find_towels(towels: list[str], pattern: str) -> list[str] | None:
@@ -20,12 +26,32 @@ def find_towels(towels: list[str], pattern: str) -> list[str] | None:
     return None
 
 
-def get_arrangement(towels: list[str], patterns: list[str]) -> list[str]:
+def get_arrangements(patterns: list[str]) -> list[str]:
     out = []
-    for pattern in patterns:
-        out.append(find_towels(towels, pattern))
+    for i, pattern in enumerate(patterns):
+        print(f"{i}/{len(patterns)}")
+        out.append(find_all_towels(pattern))
     return out
 
 
-towels, patterns = parse("input")
-print(len([a for a in get_arrangement(towels, patterns) if a]))
+@cache
+def find_all_towels(pattern: str) -> tuple[tuple[str]] | None:
+    o = []
+    for towel in towels:
+        if towel == pattern:
+            o.append(tuple(towel))
+        elif pattern.startswith(towel):
+            more_towels = find_all_towels(pattern[pattern.find(towel) + len(towel):])
+            if not more_towels:
+                continue
+            for arrangement in more_towels:
+                o.append(tuple([towel] + list(arrangement)))
+    return tuple(o)
+
+
+arrangements = get_arrangements(patterns)
+total = 0
+for arrangement in arrangements:
+    total += len(arrangement)
+    print(arrangement)
+print(total)
